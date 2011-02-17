53c4012114e9a909f1eeacde7ceab04d03a7cf58
Matt Wilson <matt@culturethree.com>
2011-02-17 14:17:10 +1300

    bump version (now with error-handling)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atompub "0.2.0-SNAPSHOT"
+(defproject atompub "0.2.1-SNAPSHOT"
   :description "An implementation of Atom Syndication and the Atom Publishing Protocol."
   :url "http://github.com/mattdw/atompub"
   :dependencies [[org.clojure/clojure "1.2.0"]
