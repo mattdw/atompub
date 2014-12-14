7fa402ffbe26732e100a879a23185fa296132577
Matt Wilson <matt@culturethree.com>
2014-12-14 23:13:11 +1300

    Documentation and formatting (core.clj)

--- src/atompub/core.clj
+++ src/atompub/core.clj
@@ -1,60 +1,116 @@
 (ns atompub.core)
 
-;; ## Feed and Entry prototypes
+;; # Atom Syndication and Publishing Protocol
 
-;; The following section contains a series of default-property maps
-;; and helper functions to create (where possible) spec-conforming
-;; atom feeds. `feed-properties`, `atom-entry` and `edit-entry` are
-;; the suggested way to create appropriate structs; they'll catch
-;; missing required fields.
+;; This library assumes you are at least passingly familiar with the
+;; Atom format and protocol. If not, start here:
+;;
+;; * <http://atomenabled.org>
+;; * <http://www.ietf.org/rfc/rfc4287.txt>
+;; * <http://bitworking.org/projects/atom/draft-ietf-atompub-protocol-04.html>
+;;
+;; This namespace defines all the necessary protocols, and helpful
+;; defrecords as shortcuts for feeds (non-editable) and entries.
+;;
+;; This library uses "feed" and "collection" in the same sense as the
+;; Atom documentation; "feed" is a single read-only XML document, and
+;; "collection" is (essentially) a feed with extra logic provided for
+;; adding, deleting, editing etc.
 
-;; ;; Default value for required fields.
-
-;; (def required-field (Exception. "Missing required field"))
-
-;; ;; ### Top-level feed properties
-
-;; (def default-feed-properties
-;;   {:title         required-field
-;;    :updated       required-field
-;;    :url           required-field    ; url to this feed
-;;    :home-url      nil               ; url to an appropriate homepage
-;;                                     ; for this feed
-;;    :author-name   required-field
-;;    :author-email  nil})
+;; ## Feeds
 
 (defprotocol IAtomFeed
-  (feed-title [this] "The feed's title.")
-  (feed-updated [this] "Must return something Joda DateTime can use.")
-  (feed-url [this] "URL that points to this feed.")
-  (feed-home-url [this] "A web link for this feed e.g. homepage. Optional, or nil.")
-  (feed-author-name [this] "The primary author's name.")
-  (feed-author-email [this] "An email for the primary author. Optional, or nil.")
-  (feed-entries [this] "Returns a list of IAtomEntry, and optionally IAtomEditableEntry."))
+
+  "This protocol is required for any type of feed; whether purely for
+  syndication, or to be exposed via the publishing protocol."
+
+  (feed-title [this]
+    "The feed's title.")
+  (feed-updated [this]
+    "Updated date/time. Can return anything that Joda DateTime can
+    use.")
+  (feed-url [this]
+    "The URL that points to this feed.")
+  (feed-home-url [this]
+    "A web link for this feed e.g. homepage. Optional, or nil.")
+  (feed-author-name [this]
+    "The primary author's name.")
+  (feed-author-email [this]
+    "An email for the primary author. Optional, or nil.")
+  (feed-entries [this]
+    "Must returns a list of IAtomEntry, and optionally
+    IAtomEditableEntry if intended as an editable collection."))
 
 (defprotocol IAtomCollection
-  (collection-category-scheme [this])
-  (collection-get-entry [this id])
-  (collection-save-entry [this id-or-nil entry])
-  (collection-delete-entry [this id]))
+
+  "This protocol must also be implemented in order to expose an
+  editable collection. Library code assumes that any object
+  implementing this protocol has also implemented IAtomFeed; that is,
+  IAtomFeed is a requirement of IAtomCollection.
+
+  The ids used by this protocol are largely opaque, but must be
+  appropriate as URL segments. That is, they must be stringifiable, and
+  this library currently won't handle anything with special characters,
+  as it does no escaping. Integers or simple strings are recommended."
+
+  (collection-category-scheme [this]
+    "A seq of strings. May be empty.")
+  (collection-get-entry [this id]
+    "Retrieve an entry by (opaque) id.")
+  (collection-save-entry [this id-or-nil entry]
+    "Save an entry. `entry` will be provided as an instance of
+    AtomEntry below. If `id-or-nil` is nil, create a new record.
+    Otherwise update an existing one.")
+  (collection-delete-entry [this id]
+    "Delete the entry at `id`."))
+
+;; ## Entries
 
 (defprotocol IAtomEntry
-  (entry-title [this])
-  (entry-id [this])
-  (entry-updated [this])
-  (entry-published [this])
-  (entry-url [this])
-  
-  (entry-content [this])
-  (entry-summary [this])
-  
-  (entry-author-name [this])
-  (entry-author-email [this]))
+
+  "IAtomEntry should be implemented for both editable and simple
+  subscription entries. However, `entry-id` should return a guid
+  (frequently a permalink) for subscription-type entries, but a simple
+  key for editable entries. (See the note in IAtomCollection regarding
+  ids.)"
+
+  (entry-title [this]
+    "The title of the entry")
+  (entry-id [this]
+    "The id of the entry. A GUID or permalink for non-editable entries,
+    or a simple key for editable entries. See notes above.")
+  (entry-updated [this]
+    "Date/time updated. As for feeds, takes anything Joda DateTime can
+    use. Commpulsory per spec.")
+  (entry-published [this]
+    "As per `updated`, and will frequently be the same. Optional per
+    spec.")
+  (entry-url [this]
+    "The canonical URL (permalink) for this entry.")
+
+  ;; For syndication, only one of these should return content, the
+  ;; other should return nil.
+
+  (entry-content [this]
+    "The entry's main content. Likely HTML for syndication, or for
+    editing whatever your raw format is e.g. Markdown, plain text,
+    HTML.")
+  (entry-summary [this]
+    "A summary or excerpt of the entry's content.")
+
+  (entry-author-name [this]
+    "The author's name. Optional.")
+  (entry-author-email [this]
+    "The author's email. Optional."))
 
 (defprotocol IAtomEditableEntry
-  (entry-draft? [this])
-  (entry-categories [this]))
+  (entry-draft? [this]
+    "Is this entry a draft? Returns boolean.")
+  (entry-categories [this]
+    "A list of categories this entry belongs to. Should use keys found
+    in `collection-category-scheme` above."))
 
+;; ## Helper defrecords
 
 (defrecord AtomFeed [title updated url home-url author-name author-email entries]
 
@@ -85,86 +141,5 @@
   IAtomEditableEntry
   (entry-draft? [this] (:draft? this))
   (entry-categories [this] (:categories this)))
-
-;; (defn feed-properties
-;;   "Create a top-level feed-properties map."
-;;   [props]
-;;   (merge default-feed-properties props))
-
-;; ;; ### Per-entry properties (syndication)
-
-;; (def default-entry-properties
-;;   {:title         required-field
-;;    :id            required-field
-;;    :updated       required-field
-;;    :published     nil
-;;    :url           nil
-;;    :content       nil
-;;    :summary       nil
-;;    :author-name   nil
-;;    :author-email  nil})
-
-;; (defn atom-entry
-;;   "Create an atom-entry map."
-;;   [entry-map]
-;;   (merge default-entry-properties entry-map))
-
-;; ;; ### Extra per-entry properties for APP protocol.
-;; ;; 
-;; ;; Extends default-entry-properties.
-
-;; (def default-edit-entry-properties
-;;   {:draft? nil
-;;    :categories nil})
-
-;; (defn edit-entry
-;;   "Create an edit-entry suitable for use with the APP protocol."
-;;   [entry-map]
-;;   (merge default-edit-entry-properties (atom-entry entry-map)))
-
-
-
-;; #_(defprotocol Feed
-;;   "Some methods can return nil to signal an ignored field. However,
-;;   some are compulsory as according to the spec."
-;;   (feed-title [this])
-;;   (feed-home-url [this])
-;;   (feed-author-name [this])
-;;   (feed-author-email [this])
-;;   (feed-updated [this])
-
-;;   ;; all entry-* methods *except for Collection/save-entry* will deal with whatever
-;;   ;; data-structure you provide via get-entr(y|ies).
-;;   (get-entries [this] "Returns a sequence of anything your entry-* methods
-;;                       can deal with.")  
-
-;;   (entry-title [this item])
-;;   (entry-id [this item])
-;;   (entry-url [this item])
-;;   (entry-published [this item])
-;;   (entry-updated [this item])
-;;   (entry-content [this item])
-;;   (entry-summary [this item])
-;;   (entry-author-name [this item])
-;;   (entry-author-email [this item])
-;;   )
-
-;; #_(defprotocol Collection
-;;   "If you implement Collection, you must also implement Feed; Collection depends
-;;    on many of Feed's methods."
-;;   (collection-url-root [this])
-;;   (collection-category-scheme [this])
-;;   (collection-categories [this])
-
-;;   (get-entry [this id])
-;;   ;; save-entry will receive a map with keys corresponding to the
-;;   ;; elements of an atom entry.
-;;   (save-entry [this id-or-nil entry])
-;;   (delete-entry [this id])
-
-;;   (entry-publish? [this item])
-;;   (entry-categories [this item])
-;;   )
-
 
 
